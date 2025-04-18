509e34e798bcf34c19537eb394083bdd4ddbfbba
agnestiw <agnestiwulansari605@gmail.com>
2025-04-18 10:58:43 +0700

    home

--- resources/views/components/back-to-top.blade.php
+++ resources/views/components/back-to-top.blade.php
@@ -1,4 +1,4 @@
-<button id="back-to-top" class="fixed bottom-6 right-6 bg-blue-600 text-white w-12 h-12 rounded-full flex items-center justify-center shadow-lg transform transition-transform duration-300 hover:scale-110 opacity-0 invisible">
+<button id="back-to-top" class="fixed bottom-6 right-6 bg-[#F0C93D] text-white w-12 h-12 rounded-full flex items-center justify-center shadow-lg transform transition-transform duration-300 hover:scale-110 opacity-0 invisible">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18" />
     </svg>
